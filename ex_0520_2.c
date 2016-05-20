853f70eb17b980c63187910845473af6896a3d5e
MasakiyoShimoda <hi14shimoda@gmail.com>
2016-05-20 11:35:12 +0900

    Create ex_0520_2.c

---
+++ ex_0520_2.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  ex_0520_1
+//
+//  Created by 下田将斉 on 2016/05/20.
+//  Copyright © 2016年 Masakiyo Shimoda. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    double s = 0.0, power;
+    int i ;
+    
+    for (i = 0; i <= 3; i++) {
+        s = s += power
+        power *= 2.0;
+    }
+    
+    printf("実行結果%f\n", s);
+    return 0;
+}
